d233c81864e0e32b51c883441f54a88fe85a8108
Flavio Ramos <flavio.ramos@gmail.com>
2014-05-30 21:22:09 -0300

    CCLog new dumpLogcat method.

--- src/main/java/com/cliqconsulting/cclib/util/CCLog.java
+++ src/main/java/com/cliqconsulting/cclib/util/CCLog.java
@@ -1,7 +1,12 @@
 package com.cliqconsulting.cclib.util;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.text.TextUtils;
 import android.util.Log;
+
+import java.io.*;
 
 /**
  * CCLog
@@ -67,5 +72,51 @@ public class CCLog {
 		mDefaultTag = defaultTag;
 	}
 
+	/**
+	 * Saves the logcat data into a text file, at the Downloads folder. The location has to be public, so
+	 * other apps are able to access. You should delete the file when it' not needed since it may contain sensitive
+	 * information, use at your own risk.
+	 *
+	 * @param context
+	 * @param handler Handles the resulting file.
+	 */
+	public static void dumpLogcat(final Context context, final CCSimpleHandler<File> handler) {
+		new AsyncTask<Object, Object, File>() {
+			@Override protected File doInBackground(Object... objects) {
+				StringBuilder log = null;
+
+				try {
+					Process process = Runtime.getRuntime().exec("logcat -d");
+					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+					log = new StringBuilder();
+					String line = "";
+					while ((line = bufferedReader.readLine()) != null) {
+						log.append(line + "\r\n");
+					}
+				} catch (Exception e) {
+					CCLog.logError(e.toString());
+				}
+
+				File file = null;
+
+				try {
+					file = new File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "log" + String.valueOf(System.currentTimeMillis()) + ".txt");
+					FileOutputStream fos = new FileOutputStream(file);
+					fos.write(log.toString().getBytes());
+					fos.close();
+				} catch (IOException e) {
+					CCLog.logError(e.toString());
+				}
+
+				return file;
+			}
+
+			@Override protected void onPostExecute(File f) {
+				if (f != null) handler.setSuccess(f);
+				else handler.setError(null);
+			}
+		}.execute();
+	}
 
 }
